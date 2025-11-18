7a40cb81ccf8c5491ee3fbca246a0055d9b0b51c
Pedro Henrique de Oliveira <pedrohenrique.oliveira119@gmail.com>
2025-11-18 19:11:04 -0300

    feat: ports

--- src/io/ports.c
+++ src/io/ports.c
@@ -1,2 +1,40 @@
 #include "ports.h"
 
+inline uint8_t inb(uint16_t port) {
+	uint8_t ret;
+	asm volatile ("inb %b0, %w1"
+					: "=a"(ret)
+					: "Nd"(port)
+					: "memory");
+	return ret;
+}
+
+inline uint16_t inw(uint16_t port) {
+	uint16_t ret;
+	asm volatile ("inw %w0, %w1"
+					: "=a"(ret)
+					: "Nd"(port)
+					: "memory");
+	return ret;
+}
+
+inline uint32_t inb(uint16_t port) {
+	uint32_t ret;
+	asm volatile ("inb %l0, %w1"
+					: "=a"(ret)
+					: "Nd"(port)
+					: "memory");
+	return ret;
+}
+
+inline void outb(uint16_t port, uint8_t data) {
+	asm volatile ("outb %w1, %b0" : : "a"(val), "Nd"(port) : "memory");	
+}
+
+inline void outw(uint16_t port, uint16_t data) {
+	asm volatile ("outb %w1, %w0" : : "a"(val), "Nd"(port) : "memory");	
+}
+
+inline void outl(uint16_t port, uint32_t data) {
+	asm volatile ("outb %w1, %l0" : : "a"(val), "Nd"(port) : "memory");	
+}
--- src/io/ports.h
+++ src/io/ports.h
@@ -2,15 +2,15 @@
 #define PORTS_H_
 #include <stdint.h>
 
-uint8_t inb(uint16_t port);
+inline uint8_t inb(uint16_t port);
 
-uint16_t inw(uint16_t port);
+inline uint16_t inw(uint16_t port);
 
-uint32_t indw(uint16_t port);
+inline uint32_t indw(uint16_t port);
 
-void outb(uint16_t port, uint8_t data);
+inline void outb(uint16_t port, uint8_t data);
 
-void outw(uint16_t port, uint16_t data);
+inline void outw(uint16_t port, uint16_t data);
 
-void outl(uint16_t port, uint32_t data);
+inline void outl(uint16_t port, uint32_t data);
 #endif
